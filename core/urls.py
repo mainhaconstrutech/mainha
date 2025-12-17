82d96b7a1d4f4744859dc2212e6cdd811159c186
mainhaconstrutech <mainhaconstrutech@gmail.com>
2025-12-17 08:20:50 -0300

    Tentativa de corrigir disponilidade de arquivos estaticos

--- core/urls.py
+++ core/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django.contrib.auth.urls')),
     path('', include('mainha.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
