b5bbbccaa6b478c7b47491e80d4dc0a186b60f31
mainhaconstrutech <mainhaconstrutech@gmail.com>
2025-12-17 08:08:44 -0300

    Correção nos host permitidos

--- core/settings.py
+++ core/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', False)
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","localhost,127.0.0.1").split(",")
 
 # Application definition
 
